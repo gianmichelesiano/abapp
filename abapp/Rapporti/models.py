115b8b13d937d973f6cd0bf38963879654ad13bc
Gianmichele Siano <gianmichele.siano@gmail.com>
2019-12-04 15:57:09 +0100

    wd

--- abapp/Rapporti/models.py
+++ abapp/Rapporti/models.py
@@ -11,23 +11,6 @@ class Relazione(models.Model):
         verbose_name_plural = 'Relazioni'
 
 
-
-class AnalisiFunzionale(models.Model):
-    specialista = models.ForeignKey('auth.User', on_delete=models.CASCADE)
-    data_creazione = models.DateTimeField(default=timezone.now)
-    antecedente = models.CharField(max_length=500, blank=True)
-    comportamento = models.CharField(max_length=500)
-    conseguenza = models.CharField(max_length=500, blank=True)
-    reazione = models.CharField(max_length=500, blank=True)
-
-    class Meta:
-        verbose_name = 'Analisi Funzionale'
-        verbose_name_plural = 'Analisi Funzionale'
-
-    def __str__(self):
-        return self.specialista.username
-
-
 class Rinforzo(models.Model):
     specialista = models.ForeignKey('auth.User', on_delete=models.CASCADE)
     data_creazione = models.DateTimeField(default=timezone.now)
@@ -50,3 +33,21 @@ class Rinforzo(models.Model):
 
     def __str__(self):
         return self.rinforzo
+        
+
+class AnalisiFunzionale(models.Model):
+    specialista = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    data_creazione = models.DateTimeField(default=timezone.now)
+    antecedente = models.CharField(max_length=500, blank=True)
+    comportamento = models.CharField(max_length=500)
+    conseguenza = models.CharField(max_length=500, blank=True)
+    reazione = models.CharField(max_length=500, blank=True)
+
+    class Meta:
+        verbose_name = 'Analisi Funzionale'
+        verbose_name_plural = 'Analisi Funzionale'
+
+    def __str__(self):
+        return self.specialista.username
+
+
