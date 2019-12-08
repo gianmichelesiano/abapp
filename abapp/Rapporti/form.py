b75d6f8a02f672d43baf7bd9a74eeb2803e99128
gianmichelesiano <gianmichele.siano@gmail.com>
2019-12-08 13:28:45 +0100

    Adding Text Area

--- abapp/Rapporti/form.py
+++ abapp/Rapporti/form.py
@@ -4,8 +4,8 @@ from django.forms import ModelForm, Textarea
 from Rapporti.models import  Relazioni
  # forms.py
 class RelazioniForm(forms.ModelForm):
-  relazione = forms.CharField( widget=forms.Textarea(attrs={'class': 'materialize-textarea','rows': 100, 'cols': 100}))
-  feedback = forms.CharField( widget=forms.Textarea(attrs={'class': 'materialize-textarea','rows': 100, 'cols': 100}))
+  relazione = forms.CharField(required=False, widget=forms.Textarea(attrs={'class': 'materialize-textarea','rows': 100, 'cols': 100}))
+  feedback = forms.CharField(required=False, widget=forms.Textarea(attrs={'class': 'materialize-textarea','rows': 100, 'cols': 100}))
   class Meta:
     model = Relazioni
     fields = ('__all__')
